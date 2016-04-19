9ab0474dbaa827e071d4d77fe9046f793daa7b18
graciemccue <graciemccue@yahoo.com>
2016-04-19 14:57:00 -0400

    lab 10

---
+++ lab 10.sql
@@ -0,0 +1,39 @@
+﻿--Grace McCue
+--19 April 2016
+
+create or replace function PreReqFor(int, refcursor) returns refcursor as
+$$
+declare 
+    courseNum int        :=$1;
+    resultset refcursor  :=$2;
+begin
+    open resultset for
+        select preReqNum
+        from prerequisites
+        where preReqNum = courseNum;
+    return resultset;
+end;
+$$
+language plpgsql;
+
+select PreReqFor(0, 'results');
+fetch all from results;
+
+
+create for replace function IsPreReqFor(Int, refcursor) returns refcursor as
+$$
+declare
+    preReqNum int         :=$1;
+    resultset refcursor   :=$2;
+begin
+    open resultset for
+        select courseNum
+        from prerequisites
+        where courseNum = preReqNum;
+    return resultset;
+end;
+$$
+language plpgsql;
+
+select IsPreReqFor(0, 'results');
+fetch all from results;
